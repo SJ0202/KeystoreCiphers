e21ddda9e0f3960cf4bf6a6997b239f89ed50a44
seongju <dnflrkrwhr5@naver.com>
2022-09-13 00:53:07 +0900

    create CryptoManager

--- app/src/main/java/com/seongju/keystoreciphers/MainActivity.kt
+++ app/src/main/java/com/seongju/keystoreciphers/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.seongju.keystoreciphers.ui.theme.KeystoreCiphersTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
---
+++ app/src/main/java/com/seongju/keystoreciphers/common/util/CryptoManager.kt
@@ -0,0 +1,96 @@
+package com.seongju.keystoreciphers.common.util
+
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import java.io.InputStream
+import java.io.OutputStream
+import java.security.KeyStore
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
+
+class CryptoManager {
+
+    private val keyStore = KeyStore.getInstance("AndroidKeyStore").apply {
+        load(null)
+    }
+
+    private val encryptCipher = Cipher.getInstance(TRANSFORMATION).apply {
+        init(Cipher.ENCRYPT_MODE, getKey())
+    }
+
+    private fun getDecryptCipherForIv(iv: ByteArray): Cipher {
+        return Cipher.getInstance(TRANSFORMATION).apply {
+            init(Cipher.DECRYPT_MODE, getKey(), IvParameterSpec(iv))
+        }
+    }
+
+    /**
+     * KyeStore의 Key확인
+     */
+    private fun getKey(): SecretKey {
+        val existingKey = keyStore.getEntry(ALIAS, null) as? KeyStore.SecretKeyEntry
+        // KeyStore에 Key가 없으면 createKey()를 통해 생성
+        return existingKey?.secretKey ?: createKey()
+    }
+
+    /**
+     * Key 생성
+     */
+    private fun createKey(): SecretKey {
+        return KeyGenerator.getInstance(ALGORITHM).apply {
+            init(
+                KeyGenParameterSpec.Builder(ALIAS, KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(BLOCK_MODE)
+                    .setEncryptionPaddings(PADDING)
+                    .setUserAuthenticationRequired(false)
+                    .setRandomizedEncryptionRequired(true)
+                    .build()
+            )
+        }.generateKey()
+    }
+
+    /**
+     * 암호화
+     */
+    fun encrypt(bytes: ByteArray, outputStream: OutputStream): ByteArray {
+        val encryptedBytes = encryptCipher.doFinal(bytes)
+        outputStream.use {
+            it.write(encryptCipher.iv.size)
+            it.write(encryptCipher.iv)
+            it.write(encryptedBytes.size)
+            it.write(encryptedBytes)
+        }
+        return encryptedBytes
+    }
+
+    /**
+     * 복호화
+     */
+    fun decrypt(inputStream: InputStream): ByteArray {
+        return inputStream.use {
+            val ivSize = it.read()
+            val iv = ByteArray(ivSize)
+            it.read(iv)
+
+            val encryptedBytesSize = it.read()
+            val encryptedBytes = ByteArray(encryptedBytesSize)
+            it.read(encryptedBytes)
+
+            getDecryptCipherForIv(iv).doFinal(encryptedBytes)
+        }
+    }
+
+    /**
+     * 암호화 옵션(AES,CBC,PKCS7)
+     */
+    companion object{
+        private const val ALGORITHM = KeyProperties.KEY_ALGORITHM_AES
+        private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
+        private const val PADDING = KeyProperties.ENCRYPTION_PADDING_PKCS7
+        private const val TRANSFORMATION = "$ALGORITHM/$BLOCK_MODE/$PADDING"
+        private const val ALIAS = "secretKey"
+    }
+
+}
